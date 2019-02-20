844c6821e52310f4b6a9cf46a83edd95ed130a2b
DaiLinBo(戴林波) <DaiLinBo@bakheet.cn>
2019-02-20 09:56:13 +0800

    配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+// 创建一个插件的实例对象
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: path.join(__dirname, './src/index.html'),
+  filename: 'index.html' // 生成的内存中首页的名称
+})
 module.exports = {
-  mode: 'production' //development production
+  mode: 'development', //development production
+  plugins: [
+    htmlPlugin
+  ]
 }
